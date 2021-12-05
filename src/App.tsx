4c438fecf09dd1cacf940d1316bd13e356de1491
Chandragupt Gosavi <chandraguptgosavi@gmail.com>
2021-12-05 21:44:46 +0530

    navbabar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
 function App() {
+  const navbarEl = useRef<null | HTMLElement>(null);
+  const [prevScrollPos, setPrevScrollPos] = useState<number>(window.scrollY);
+
+  window.addEventListener("scroll", () => {
+    const currScrollPos: number = window.scrollY;
+
+    if (navbarEl.current) {
+      if (prevScrollPos > currScrollPos) {
+        navbarEl.current.style.top = "0";
+      } else {
+        navbarEl.current.style.top = "-4rem";
+      }
+      setPrevScrollPos(currScrollPos);
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="w-full 
+      bg-navy 
+      text-slate font-serif"
+    >
+      <nav
+        ref={navbarEl}
+        className="z-50
+          fixed top-0 
+          h-16 w-full
+          bg-navy text-lightestSlate
+          navbar-shadow
+          transition-top duration-300
+          flex justify-between items-center
+          "
+      >
+        <div className="ml-2"></div>
+        <div className="mr-2 font-mono flex items-center">
+          <ul
+            className="list-none
+           flex justify-evenly"
+          >
+            <a href="#about">
+              <li className="mx-3 text-sm hover:text-green">About</li>
+            </a>
+            <a href="#skills">
+              <li className="mx-3 text-sm hover:text-green">Skills</li>
+            </a>
+            <a href="#projects">
+              <li className="mx-3 text-sm hover:text-green">Projects</li>
+            </a>
+            <a href="#connect" className="md:hidden">
+              <li className="mx-3 text-sm hover:text-green">Connect</li>
+            </a>
+          </ul>
+          <div
+            className="mx-3
+          px-4 py-2
+          border-2 border-green
+          rounded-md
+          text-sm text-green
+          cursor-pointer"
+          >
+            Resume
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Inter", "Rubik", "sans-serif"],
+        mono: ["Fira Code", "Source Code Pro", "monospace"],
+      },
+      transitionProperty: {
+        top: "top",
+      },
+      colors: {
+        navy: "#0a192f",
+        lightNavy: "#112240",
+        lightestNavy: "#233554",
+        slate: "#8892b0",
+        lightSlate: "#a8b2d1",
+        lightestSlate: "#ccd6f6",
+        white: "#e6f1ff",
+        green: "#64ffda",
+      },
+      inset: {
+        "2/100": "2%",
+        "3/100": "3%"
+      },
+      height: {
+        "11/12":"91.666667%",
+      }
+    },
+    screens: {
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
